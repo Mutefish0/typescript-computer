9abe95ffaa1c3e965398312484a8dccc6ef772ff
chengyifan <chengyifan@taou.com>
2022-05-23 01:25:51 +0800

    update

--- README.md
+++ README.md
@@ -2,46 +2,74 @@
 
 Computations with typescrip types
 
-## Math
+// Type convert
 
 ```ts
-type DEMO1 = $Add<3, 4>; // 7
-type DEMO2 = $Sub<9, 2>; // 7
-type DEMO3 = $Sub<4, 7>; // -3
+type DEMO0 = $$Number<"8">["value"]; // 8
+type DEMO00 = $$Number<8>["print"]; // "8"
 ```
 
-## Logical
+// Math
+
+```ts
+type DEMO1 = $Add<3, 4>["print"]; // 7
+type DEMO2 = $Subtract<9, 2>["print"]; // 7
+type DEMO3 = $Subtract<4, 7>["print"]; // -3
+type DEMO4 = $Multiply<4, 8>["print"]; // 32
+type DEMO6 = $Divide<24, 3>["print"]; // 8
+```
+
+// Logical
 
 ```ts
 type DEMO_4_gt_5 = $GT<4, 5>; // 0
 type DEMO_6_gt_3 = $GT<6, 3>; // 1
 type DEMO_4_lt_5 = $LT<4, 5>; // 1
 type DEMO_6_lt_3 = $LT<6, 3>; // 0
-```
 
-```ts
 type DEMO_0_eq_0 = $EQZ<0>; // 1
 type DEMO_1_eq_0 = $EQZ<1>; // 0
 type DEMO_and = $AND<DEMO_0_eq_0, DEMO_1_eq_0>; // 0
 type DEMO_or = $OR<DEMO_0_eq_0, DEMO_1_eq_0>; // 1
 ```
 
-## Functions
+// Functions
 
-### reduce_sum
+// fibonacci
 
 ```ts
-type DEMO_reduce_sum_2 = Call<"reduce_sum", 2>; // 1 + 2 = 3
-type DEMO_reduce_sum_3 = Call<"reduce_sum", 3>; // 1 + 2 + 3 = 6
-type DEMO_reduce_sum_5 = Call<"reduce_sum", 5>; // 1 + 2 + 3 + 4 = 10
-type DEMO_reduce_sum_6 = Call<"reduce_sum", 6>; // 1 + 2 + 3 + 4 + 5 + 6 = 21
+type Fibonacci<
+  I extends number,
+  Prev1 extends number = 1,
+  Prev2 extends number = 0,
+  Result extends string = "1"
+> = I extends 0 | 1
+  ? Result
+  : I extends number
+  ? Fibonacci<
+      $Subtract<I, 1>["value"],
+      $Add<Prev1, Prev2>["value"],
+      Prev1,
+      `${Result}, ${$Add<Prev1, Prev2>["value"]}`
+    >
+  : 0;
+
+type F0 = Fibonacci<10>; // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 ```
 
-### fibonacci
+// reduceSum
 
 ```ts
-type DEMO_fibonacci_4 = Call<"fibonacci", 4>; // 1, 1, 2, 3
-type DEMO_fibonacci_6 = Call<"fibonacci", 6>; // 1, 1, 2, 3, 5, 8
-type DEMO_fibonacci_7 = Call<"fibonacci", 7>; // 1, 1, 2, 3, 5, 8, 13
-type DEMO_fibonacci_8 = Call<"fibonacci", 8>; // 1, 1, 2, 3, 5, 8, 13, 21
+type ReduceSum<
+  X extends number,
+  I extends number = 0,
+  Result extends number = 0
+> = I extends $Add<X, 1>["value"]
+  ? Result
+  : ReduceSum<X, $Add<I, 1>["value"], $Add<Result, I>["value"]>;
+
+type R3 = ReduceSum<3>; // 1 + 2 + 3  = 5
+type R4 = ReduceSum<4>; // 1 + 2 + 3 +  5 = 10
+type R13 = ReduceSum<12>; // 78
+type Calc13 = $Divide<$Multiply<$Add<1, 12>, 12>, 2>["value"]; // (x + 1) * x / 2 = 78
 ```
---
+++ core.ts
@@ -0,0 +1,282 @@
+type _MAX_NUMBER = 200;
+type _Index_Max_Number_Array = _NArrayNumber<_MAX_NUMBER>;
+type _Index_Max_Number = _Index_Max_Number_Array[number];
+type _Index_Max_Number_String_Pos = `${_Index_Max_Number}`;
+type _Index_Max_Number_String_Neg = Exclude<
+  `-${_Index_Max_Number_String_Pos}`,
+  "-0"
+>;
+
+type _RemoveSign<T extends _Index_Max_Number_String_Neg> =
+  T extends `-${infer U}`
+    ? U extends _Index_Max_Number_String_Pos
+      ? U
+      : never
+    : never;
+
+type _NArray<T, N extends number> = N extends N
+  ? number extends N
+    ? T[]
+    : __NArray<T, N, []>
+  : never;
+
+type _Length<X extends any[]> = X["length"] extends infer U
+  ? U extends number
+    ? U
+    : 0
+  : 0;
+
+type __NArray<T, N extends number, R extends unknown[]> = _Length<R> extends N
+  ? R
+  : __NArray<T, N, [...R, _Length<R>]>;
+
+type _NArrayNumber<L extends number> = _NArray<number, L>;
+
+type $CastType<X, Y, Default extends Y> = X extends Y ? X : Default;
+
+// 遍历数组
+type _ArrayStuct<T, Head extends T, Tail extends T[]> = [Head, ...Tail];
+
+type $Bool = 1 | 0; // True & False
+
+type $AND<X extends $Bool, Y extends $Bool> = {
+  0: 0;
+  1: X;
+}[Y];
+
+type $OR<X extends $Bool, Y extends $Bool> = {
+  0: Y;
+  1: 1;
+}[X];
+
+type $NOT<X extends $Bool> = {
+  0: 1;
+  1: 0;
+}[X];
+
+// 异或
+type $XOR<X extends $Bool, Y extends $Bool> = {
+  0: $AND<X, 1>;
+  1: $NOT<$OR<X, 0>>;
+}[Y];
+
+// 同或
+type $EQV<X extends $Bool, Y extends $Bool> = $NOT<$XOR<X, Y>>;
+
+type $IFELSE<B extends $Bool, IF, ELSE> = {
+  1: IF;
+  0: ELSE;
+}[B];
+
+type $Join<
+  Arr extends any[],
+  Sp extends string,
+  Result extends string = ""
+> = Arr extends []
+  ? Result
+  : Arr extends _ArrayStuct<Arr[0], infer Word, []>
+  ? `${Result}${Word}`
+  : Arr extends _ArrayStuct<Arr[0], infer Word, infer Tail>
+  ? $Join<Tail, Sp, `${Result}${Word}${Sp}`>
+  : never;
+
+type $Error<Op extends string, Args extends unknown[], Msg extends string> = {
+  op: `${Op}<${$Join<Args, ", ">}>`;
+  error: `${Op}<${$Join<Args, ", ">}>: ${Msg}`;
+};
+
+type $Number = {
+  sign: $Bool;
+  value: _Index_Max_Number;
+  print: string;
+};
+
+type $PosNumber = {
+  sign: 0;
+  value: _Index_Max_Number;
+  print: string;
+};
+
+type $NegNumber = {
+  sign: 1;
+  value: _Index_Max_Number;
+  print: string;
+};
+
+type $ReverseSign<X extends $Number> = {
+  sign: $NOT<X["sign"]>;
+  value: X["value"];
+  print: $IFELSE<X["sign"], `${X["value"]}`, `-${X["value"]}`>;
+};
+
+type $SetSign<X extends $Number, S extends $Bool> = {
+  sign: S;
+  value: X["value"];
+  print: $IFELSE<S, `-${X["value"]}`, `${X["value"]}`>;
+};
+
+type $Abs<X extends $Number> = $SetSign<X, 0>;
+
+type _Number<T extends string> = T extends _Index_Max_Number_String_Pos
+  ? {
+      value: _Index_Max_Number_Array[T];
+      sign: 0;
+      print: T;
+    }
+  : T extends _Index_Max_Number_String_Neg
+  ? {
+      sign: 1;
+      value: _Index_Max_Number_Array[_RemoveSign<T>];
+      print: T;
+    }
+  : _Number<"0">;
+//$Error<"$Number", [T], `excced max number ${_MAX_NUMBER}`>;
+
+type $$Number<T extends number | string> = T extends string
+  ? _Number<T>
+  : T extends number
+  ? _Number<`${T}`>
+  : $$Number<0>;
+
+type __SubtractPos<
+  X extends _Index_Max_Number,
+  Y extends _Index_Max_Number,
+  SET extends $Bool = 0
+> = _NArrayNumber<X> extends [..._NArrayNumber<Y>, ...infer R]
+  ? $SetSign<$$Number<_Length<R>>, SET>
+  : $$Number<-1>;
+
+type _SubtractPos<
+  X extends _Index_Max_Number,
+  Y extends _Index_Max_Number
+> = X extends Y
+  ? $$Number<0>
+  : __SubtractPos<X, Y> extends infer U
+  ? U extends $$Number<-1>
+    ? __SubtractPos<Y, X, 1>
+    : U
+  : $$Number<-1>;
+
+type _AddPos<
+  X extends _Index_Max_Number,
+  Y extends _Index_Max_Number
+> = _Length<[..._NArrayNumber<X>, ..._NArrayNumber<Y>]>;
+
+// 乘法(尾递归优化)
+type _MultiplyPos<
+  X extends _Index_Max_Number,
+  Y extends _Index_Max_Number,
+  Result extends _Index_Max_Number = 0
+> = Y extends 0
+  ? Result
+  : _MultiplyPos<
+      X,
+      $CastType<_SubtractPos<Y, 1>, $PosNumber, $$Number<0>>["value"],
+      _AddPos<Result, X>
+    >;
+
+// 除法(尾递归优化)
+type _DividePos<
+  X extends _Index_Max_Number,
+  Y extends _Index_Max_Number,
+  Result extends _Index_Max_Number = 0
+> = Y extends 0
+  ? $Error<"_DividePos", [X, Y], "divide by zero">
+  : Y extends 1
+  ? X
+  : _SubtractPos<X, Y> extends infer U
+  ? U extends $Number
+    ? U["value"] extends 0
+      ? _AddPos<Result, 1>
+      : U["sign"] extends 1
+      ? Result
+      : _DividePos<U["value"], Y, _AddPos<Result, 1>>
+    : 0
+  : 0;
+
+type $EQZ<
+  X extends $Number | number,
+  X1 extends $Number = X extends $Number ? X : $$Number<$CastType<X, number, 0>>
+> = X1["value"] extends 0 ? 1 : 0;
+type $GTZ<X extends $Number> = $NOT<X["sign"]>;
+type $LTZ<X extends $Number> = X["sign"];
+
+// 加法
+type $Add<
+  X extends $Number | number,
+  Y extends $Number | number,
+  X1 extends $Number = X extends $Number
+    ? X
+    : $$Number<$CastType<X, number, 0>>,
+  Y1 extends $Number = Y extends $Number ? Y : $$Number<$CastType<Y, number, 0>>
+> = $EQV<X1["sign"], Y1["sign"]> extends 1
+  ? $SetSign<$$Number<_AddPos<X1["value"], Y1["value"]>>, X1["sign"]>
+  : _SubtractPos<X1["value"], Y1["value"]> extends infer U
+  ? U extends $Number
+    ? $SetSign<U, $AND<$XOR<X1["sign"], U["sign"]>, $NOT<$EQZ<U>>>>
+    : $$Number<0>
+  : $$Number<0>;
+
+// 减法
+type $Subtract<
+  X extends $Number | number,
+  Y extends $Number | number,
+  X1 extends $Number = X extends $Number
+    ? X
+    : $$Number<$CastType<X, number, 0>>,
+  Y1 extends $Number = Y extends $Number ? Y : $$Number<$CastType<Y, number, 0>>
+> = $Add<X1, $ReverseSign<Y1>>;
+
+type $EQ<X extends $Number, Y extends $Number> = X["value"] extends Y["value"]
+  ? 1
+  : 0;
+
+type $GT<
+  X extends $Number | number,
+  Y extends $Number | number,
+  X1 extends $Number = X extends $Number
+    ? X
+    : $$Number<$CastType<X, number, 0>>,
+  Y1 extends $Number = Y extends $Number ? Y : $$Number<$CastType<Y, number, 0>>
+> = $GTZ<$CastType<$Subtract<X1, Y1>, $Number, $$Number<0>>>;
+
+type $LT<
+  X extends $Number | number,
+  Y extends $Number | number,
+  X1 extends $Number = X extends $Number
+    ? X
+    : $$Number<$CastType<X, number, 0>>,
+  Y1 extends $Number = Y extends $Number ? Y : $$Number<$CastType<Y, number, 0>>
+> = $LTZ<$CastType<$Subtract<X1, Y1>, $Number, $$Number<0>>>;
+
+// 乘法
+type $Multiply<
+  X extends $Number | number,
+  Y extends $Number | number,
+  X1 extends $Number = X extends $Number
+    ? X
+    : $$Number<$CastType<X, number, 0>>,
+  Y1 extends $Number = Y extends $Number ? Y : $$Number<$CastType<Y, number, 0>>
+> = $SetSign<
+  {
+    value: _MultiplyPos<X1["value"], Y1["value"]>;
+    sign: 0;
+    print: "";
+  },
+  $XOR<X1["sign"], Y1["sign"]>
+>;
+
+// 除法
+type $Divide<
+  X extends $Number | number,
+  Y extends $Number | number,
+  X1 extends $Number = X extends $Number
+    ? X
+    : $$Number<$CastType<X, number, 0>>,
+  Y1 extends $Number = Y extends $Number ? Y : $$Number<$CastType<Y, number, 0>>
+> = $SetSign<
+  $$Number<
+    $CastType<_DividePos<X1["value"], Y1["value"]>, _Index_Max_Number, 0>
+  >,
+  $XOR<X1["sign"], Y1["sign"]>
+>;
